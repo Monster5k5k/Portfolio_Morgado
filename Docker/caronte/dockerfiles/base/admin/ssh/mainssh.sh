0a312f1e6ec5e8dc9a90b1ef066403ecd8660c4b
Monster5k5k <linascijunskisdodonova4@gmail.com>
2025-11-19 12:03:20 +0100

    cambio para tener la clave de ssh

--- Docker/caronte/dockerfiles/base/admin/ssh/mainssh.sh
+++ Docker/caronte/dockerfiles/base/admin/ssh/mainssh.sh
@@ -7,10 +7,21 @@ configurar_ssh() {
 
   # 2. Cambiar el puerto de SSH
   sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
-  # 3. Configurar la autenticación por clave pública
-  mkdir -p /var/run/sshd
-  ssh-keygen -A
+  # 3. Configurar las claves SSH para el usuario
+  if [ ! d /home/$USUARIO/.ssh ]; then
+    mkdir -p /home/$USUARIO/.ssh
+    cat /root/datos/id_rsa.pub >> /home/$USUARIO/.ssh/authorized_keys
+  fi
   # 4. Reiniciar el servicio SSH para que se aplique las configuraciones
   service ssh restart
-
+  #exec /usr/sbin/sshd -D & #dejar el ssh en background /(2plano)
+  mkdir /home/$USUARIO/.ssh
+  cat /root/admin/base/common/id_ed25519.pub >> /home/$USUARIO/.ssh/authorized_keys
+  config_sudoers () {
+    if [ -f /etc/sudoers ]
+    then
+        #comprobar que el ${USUARIO} No existe
+        echo "${USUARIO} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+      fi
+  }
 }
