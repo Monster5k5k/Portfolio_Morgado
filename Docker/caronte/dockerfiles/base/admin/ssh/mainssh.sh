961b26e314004a81a119312f7b3b6525d62f4d6e
Monster5k5k <linascijunskisdodonova4@gmail.com>
2025-11-19 13:43:02 +0100

    fin fin fin

--- Docker/caronte/dockerfiles/base/admin/ssh/mainssh.sh
+++ Docker/caronte/dockerfiles/base/admin/ssh/mainssh.sh
@@ -7,16 +7,11 @@ configurar_ssh() {
 
   # 2. Cambiar el puerto de SSH
   sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
-  # 3. Configurar las claves SSH para el usuario
-  if [ ! d /home/$USUARIO/.ssh ]; then
-    mkdir -p /home/$USUARIO/.ssh
-    cat /root/admin/base/common/id_rsa.pub >> /home/$USUARIO/.ssh/authorized_keys
-  fi
-  # 4. Reiniciar el servicio SSH para que se aplique las configuraciones
+
   service ssh restart
   #exec /usr/sbin/sshd -D & #dejar el ssh en background /(2plano)
-  mkdir /home/$USUARIO/.ssh
-  cat /root/admin/base/common/id_rsa.pub >> /home/$USUARIO/.ssh/authorized_keys
+  mkdir /home/${USUARIO}/.ssh
+  cat /root/admin/base/common/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys
 
   config_sudoers () {
     if [ -f /etc/sudoers ]
